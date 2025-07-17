172b635273c4ca5218230c5091f5e7b2422914ee
kilfour <kilfour@gmail.com>
2025-07-17 23:28:13 +0200

    formatting

--- QuickPulse.Show/FlowContext.cs
+++ QuickPulse.Show/FlowContext.cs
@@ -17,6 +17,7 @@ public record FlowContext
     public bool NeedsIndent { get; init; } = false;
     public int Level { get; init; } = 0;
     private readonly HashSet<object> visited = new(ReferenceEqualityComparer.Instance);
+
     public bool AlreadyVisited(object obj)
     {
         if (obj == null || obj.GetType().IsValueType) return false; // just to be on the safe side
