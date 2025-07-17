a2d8a07f8886cf2e0da5f3c3d3f8f5b3f5eeaf07
kilfour <kilfour@gmail.com>
2025-07-17 23:16:23 +0200

    test added

---
+++ QuickPulse.Show.Tests/CycleStructTests.cs
@@ -0,0 +1,16 @@
+namespace QuickPulse.Show.Tests;
+
+public class CycleStructTests
+{
+    private class Node { public object? Value { get; set; } }
+    private struct Wrapper { public Node Node; }
+
+    [Fact]
+    public void Does_not_stack_overflow_on_struct_in_reference_cycle()
+    {
+        var node = new Node();
+        var wrapper = new Wrapper { Node = node };
+        node.Value = wrapper;
+        Introduce.This(node);
+    }
+}
