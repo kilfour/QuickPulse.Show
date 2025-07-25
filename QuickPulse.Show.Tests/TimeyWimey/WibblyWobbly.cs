8c610b86fe7687b4254519ecc21b7ea26ec27b1d
kilfour <kilfour@gmail.com>
2025-07-25 20:48:04 +0200

    timey whimey

--- QuickPulse.Show.Tests/TimeyWimey/WibblyWobbly.cs
+++ QuickPulse.Show.Tests/TimeyWimey/WibblyWobbly.cs
@@ -1,3 +1,4 @@
+using QuickPulse.Instruments;
 using QuickPulse.Show.TimeyWimey;
 
 namespace QuickPulse.Show.Tests.TimeyWimey;
@@ -5,14 +6,62 @@ namespace QuickPulse.Show.Tests.TimeyWimey;
 public class WibblyWobbly
 {
     [Fact]
-    public void HumanDate_To_DateTime()
+    public void HumanDate_To_DateOnly()
     {
-        Assert.Equal(new DateTime(2025, 1, 20), 20.January(2025));
+        Assert.Equal(new DateOnly(2025, 1, 20), 20.January(2025));
     }
 
-    // [Fact]
-    // public void HumanDate_With_Hours()
-    // {
-    //     Assert.Equal(new DateTime(2025, 1, 20, 10, 0, 0), 20.January(2025, 10.Hours()));
-    // }
+    [Fact]
+    public void HumanDate_To_DateTime()
+    {
+        Assert.Equal(new DateTime(2025, 1, 20, 10, 0, 0), 20.January(2025).At(10.OClock()));
+    }
+
+    [Fact]
+    public void HumanTime_OClock()
+    {
+        Assert.Equal(new TimeOnly(5, 0, 0), 5.OClock());
+
+        Assert.Throws<ComputerSaysNo>(() => 0.OClock());
+        Assert.Throws<ComputerSaysNo>(() => 13.OClock());
+
+        Assert.Throws<ComputerSaysNo>(() => (-2).OClock());
+        Assert.Throws<ComputerSaysNo>(() => 100.OClock());
+    }
+
+    [Fact]
+    public void HumanTime_To()
+    {
+        Assert.Equal(new TimeOnly(11, 50, 0), 10.To(12));
+
+        Assert.Throws<ComputerSaysNo>(() => 10.To(17));
+        Assert.Throws<ComputerSaysNo>(() => 10.To(0));
+
+        Assert.Throws<ComputerSaysNo>(() => 0.To(2));
+        Assert.Throws<ComputerSaysNo>(() => 60.To(2));
+
+        Assert.Throws<ComputerSaysNo>(() => 1.To(-2));
+        Assert.Throws<ComputerSaysNo>(() => 59.To(100));
+
+        Assert.Throws<ComputerSaysNo>(() => (-2).To(2));
+        Assert.Throws<ComputerSaysNo>(() => 100.To(2));
+    }
+
+    [Fact]
+    public void HumanTime_Past()
+    {
+        Assert.Equal(new TimeOnly(11, 20, 0), 20.Past(11));
+
+        Assert.Throws<ComputerSaysNo>(() => 10.Past(13));
+        Assert.Throws<ComputerSaysNo>(() => 10.Past(-2));
+
+        Assert.Throws<ComputerSaysNo>(() => (-2).Past(2));
+        Assert.Throws<ComputerSaysNo>(() => 100.Past(2));
+    }
+
+    [Fact]
+    public void PM()
+    {
+        Assert.Equal(new TimeOnly(17, 0), 5.OClock().PM());
+    }
 }
--- QuickPulse.Show/TimeyWimey/WibblyWobbly.cs
+++ QuickPulse.Show/TimeyWimey/WibblyWobbly.cs
@@ -1,27 +1,29 @@
+using QuickPulse.Instruments;
+
 namespace QuickPulse.Show.TimeyWimey;
 
 public static class WibblyWobbly
 {
     // , TimeOnly? time = null
-    public static DateTime January(this int day, int year) { return new DateTime(year, 1, day); }
-    public static DateTime February(this int day, int year) { return new DateTime(year, 2, day); }
-    public static DateTime March(this int day, int year) { return new DateTime(year, 3, day); }
-    public static DateTime April(this int day, int year) { return new DateTime(year, 4, day); }
-    public static DateTime May(this int day, int year) { return new DateTime(year, 5, day); }
-    public static DateTime June(this int day, int year) { return new DateTime(year, 6, day); }
-    public static DateTime July(this int day, int year) { return new DateTime(year, 7, day); }
-    public static DateTime August(this int day, int year) { return new DateTime(year, 8, day); }
-    public static DateTime September(this int day, int year) { return new DateTime(year, 9, day); }
-    public static DateTime October(this int day, int year) { return new DateTime(year, 10, day); }
-    public static DateTime November(this int day, int year) { return new DateTime(year, 11, day); }
-    public static DateTime December(this int day, int year) { return new DateTime(year, 12, day); }
+    public static DateOnly January(this int day, int year) { return new DateOnly(year, 1, day); }
+    public static DateOnly February(this int day, int year) { return new DateOnly(year, 2, day); }
+    public static DateOnly March(this int day, int year) { return new DateOnly(year, 3, day); }
+    public static DateOnly April(this int day, int year) { return new DateOnly(year, 4, day); }
+    public static DateOnly May(this int day, int year) { return new DateOnly(year, 5, day); }
+    public static DateOnly June(this int day, int year) { return new DateOnly(year, 6, day); }
+    public static DateOnly July(this int day, int year) { return new DateOnly(year, 7, day); }
+    public static DateOnly August(this int day, int year) { return new DateOnly(year, 8, day); }
+    public static DateOnly September(this int day, int year) { return new DateOnly(year, 9, day); }
+    public static DateOnly October(this int day, int year) { return new DateOnly(year, 10, day); }
+    public static DateOnly November(this int day, int year) { return new DateOnly(year, 11, day); }
+    public static DateOnly December(this int day, int year) { return new DateOnly(year, 12, day); }
 
 
-    public static string ToHumanDateTime(this DateTime datetime) =>
-        $"{datetime.Day}.{months[datetime.Month]}({datetime.Year})"; // TimeOnly.FromDateTime(date)
+    public static string ToHumanDateOnly(this DateOnly datetime) =>
+        $"{datetime.Day}.{months[datetime.Month]}({datetime.Year})";
 
-    public static string ToHumanDate(this DateOnly date) =>
-        $"{date.Day}.{months[date.Month]}({date.Year})";
+    // public static string ToHumanDate(this DateOnly date) =>
+    //     $"{date.Day}.{months[date.Month]}({date.Year})";
 
     public static string ToHumanTime(this TimeOnly time)
     {
@@ -31,12 +33,12 @@ public static class WibblyWobbly
         var ms = time.Millisecond;
 
         if (s == 0 && ms == 0)
-            return $"{h}.Hours({m:D2})";
+            return $"{h}.OClock({m:D2})";
 
         if (ms == 0)
-            return $"{h}.Hours({m:D2}.Minutes({s:D2}))";
+            return $"{h}.OClock({m:D2}.Minutes({s:D2}))";
 
-        return $"{h}.Hours({m:D2}.Minutes({s:D2}.Seconds({ms:D3})))";
+        return $"{h}.OClock({m:D2}.Minutes({s:D2}.Seconds({ms:D3})))";
     }
 
     private static readonly Dictionary<int, string> months =
@@ -56,8 +58,34 @@ public static class WibblyWobbly
             {12, "December"},
         };
 
-    // public static TimeOnly Hours(this int hours)
-    // {
-    //     return new TimeOnly(hours, 0);
-    // }
+    public static DateTime At(this DateOnly dateOnly, TimeOnly timeOnly)
+    {
+        return dateOnly.ToDateTime(timeOnly);
+    }
+
+    public static TimeOnly OClock(this int hours)
+        => new TimeOnly(CheckHour(hours), 0);
+
+    public static TimeOnly PM(this TimeOnly timeOnly)
+        => timeOnly.Add(new TimeSpan(12, 0, 0));
+
+    public static TimeOnly To(this int minutes, int hours)
+        => new TimeOnly(CheckHour(hours), 0).Add(new TimeSpan(0, 0 - CheckMinutes(minutes), 0));
+
+    public static TimeOnly Past(this int minutes, int hours)
+        => new TimeOnly(CheckHour(hours), CheckMinutes(minutes));
+
+    private static int CheckHour(int hour)
+    {
+        if (hour < 1) ComputerSays.No("Hour must be bigger than zero.");
+        if (hour > 12) ComputerSays.No("Hour must be smaller than 13.");
+        return hour;
+    }
+
+    private static int CheckMinutes(int hour)
+    {
+        if (hour < 1) ComputerSays.No("Minutes must be bigger than zero.");
+        if (hour > 59) ComputerSays.No("Hour must be smaller than 60.");
+        return hour;
+    }
 }
